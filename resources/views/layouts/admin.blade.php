7e2cd55e2908cbedba95f8932749a13c2999e9a8
Your Name <you@example.com>
2019-05-23 11:11:25 +0800

    preventback

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -41,7 +41,12 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
-
+      <script type = "text/javascript" >
+         function preventBack(){window.history.forward();}
+          setTimeout("preventBack()", 0);
+          window.onunload=function(){null};
+          
+      </script>
     
 
 
